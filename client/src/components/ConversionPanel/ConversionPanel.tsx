82d6279e063880c7d2d8e38339b13aaa1071be43
rrtry <fedormocalov36@gmail.com>
2026-05-22 00:24:01 +0300

    removed unused assets

--- client/src/components/ConversionPanel/ConversionPanel.tsx
+++ client/src/components/ConversionPanel/ConversionPanel.tsx
@@ -28,7 +28,6 @@ export const ConversionPanel = ({
           <div className={styles.separator}></div>
           <div className={styles.currency}>{toCurrency}</div>
         </div>
-
       </div>
     </div>
   );
