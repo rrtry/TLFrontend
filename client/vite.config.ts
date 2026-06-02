c500f19c13ac10011f31471e5ff4a59e7a70468d
rrtry <fedormocalov36@gmail.com>
2026-06-02 12:22:20 +0300

    Update configs for hw2

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
@@ -9,4 +8,9 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
   },
+  css: {
+    modules: {
+        localsConvention: 'camelCaseOnly'
+    }
+  }
 });
