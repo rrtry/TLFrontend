60da794d8c1d0bf77db267afa2837b773bfb05e9
rrtry <fedormocalov36@gmail.com>
2026-06-16 00:59:13 +0300

    Removed unused files

--- client/src/mocks/currencies.ts
+++
@@ -1,4 +0,0 @@
-import type { Currency } from '../models/Currency';
-import mockCurrencies from './2_hw_mock_currencies.json';
-
-export const currencies: Currency[] = mockCurrencies as Currency[];
--- client/src/mocks/priceChanges.ts
+++
@@ -1,4 +0,0 @@
-import type { PriceChange } from '../models/PriceChange';
-import rawPriceChanges from './2_hw_mock_price_changes.json';
-
-export const priceChanges: Record<string, Record<string, PriceChange>> = rawPriceChanges as Record<string, Record<string, PriceChange>>;
